6168d03e2b9d6d92092bfc4dba958e6ccc3d9f28
Erick Silva <erick.silva@firstdecision.com.br>
2021-02-18 12:35:17 -0300

    feat: first routes + env config

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// 3rd party packages
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// Variables
+const app = express();
+
+// Path imports
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorRoutes = require('./routes/error');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use(errorRoutes);
+
+// Server
+app.listen(process.env.PORT);
